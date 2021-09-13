fd8281f4cb9d0290d9df5162827d6bc6dd024b11
riyananas002 <gudangitdotnet@gmail.com>
2021-09-13 12:24:34 +0700

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='UTF-8' />
+    <meta name='viewport' content='width=device-width, initial-scale=1' />
+    <meta http-equiv='x-ua-compatible' content='ie=edge' />
+    <link rel="stylesheet" href="css/style.css" />
+    <title><strong>GudangIT.net</strong> | Posts</title>
+
+</head>
+<body>
+<h1>Halaman Posts</h1>
+<script src="js/script.js"></script>
+</body>
+</html>
